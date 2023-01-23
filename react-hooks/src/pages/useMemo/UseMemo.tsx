0c7faa28f371d0f977d5b81aebb3d24a5d96aa96
Nikola Milovanovic <nikolam998@gmail.com>
2023-01-24 00:09:55 +0100

    memo

--- react-hooks/src/pages/useMemo/UseMemo.tsx
+++ react-hooks/src/pages/useMemo/UseMemo.tsx
@@ -1,3 +1,41 @@
+import { useState, useMemo, useEffect } from "react";
+import { UseEffect } from "../useEffect/UseEffect";
+
 export function UseMemo() {
-  return <></>;
+  const [number, setNumber] = useState(0);
+  const [dark, setDark] = useState(false);
+  const doubleNumber = useMemo(() => {
+    return slowFunction(number);
+  }, [number]);
+  const themeStyles = useMemo(() => {
+    return {
+      backgroundColor: dark ? "black" : "white",
+      color: dark ? "white" : "black",
+    };
+  }, [dark]);
+
+  useEffect(() => {
+    console.log("theme changed");
+  }, [themeStyles]);
+  return (
+    <>
+      <input
+        type="number"
+        value={number}
+        onChange={(e) => setNumber(parseInt(e.target.value))}
+      />
+      <button onClick={() => setDark((prevDark) => !prevDark)}>
+        Change Theme
+      </button>
+      <div style={themeStyles}>{doubleNumber}</div>
+    </>
+  );
+}
+
+function slowFunction(number: number) {
+  console.log("Calling Slow Function");
+
+  for (let i = 0; i <= 1000000000; i++) {}
+
+  return number * 2;
 }
